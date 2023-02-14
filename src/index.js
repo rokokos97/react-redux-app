30b76f9cbbdd67549cdd93cf42c4cc00ba72603e
Rostyslav Lisovyi <rokokos97@gmail.com>
2023-02-14 12:03:06 +0100

    index.js =>  add more tasks

--- src/index.js
+++ src/index.js
@@ -1,21 +1,61 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-function createStore(initialState) {
+function taskReducer (state,action){
+    switch(action.type) {
+    case "task/completed":
+        const newArray = [...state]
+        const elementIndex = newArray.findIndex((e) => e.id === action.payload.id)
+        newArray[elementIndex].completed = true;
+        return newArray
+    default:
+        break;
+    }
+}
+
+function createStore(reducer,initialState) {
     let state = initialState;
     function getState(){
         return state
     }
     function dispatch(action){
-
+        state = reducer(state, action)
     }
     return {getState, dispatch}
 }
-const store = createStore([{id:1, description: "Task 1", status: false}]);
+const store = createStore(taskReducer,[{id:1, description: "Task 1", completed: false},
+    {id:2, description: "Task 2", completed: false}]);
 const App = () => {
+    const state = store.getState()
+    const taskComplete = (taskId) => {
+        store.dispatch({
+            type: "task/completed",
+            payload: {id: taskId}
+        })
+        console.log(store.getState());
+    }
     console.log(store.getState());
-    store.dispatch()
-    return <h1>App</h1>
+    return (<>
+            <h1>App</h1>
+
+            <ul>
+                {state.map((el) =>
+                    <li key={el.id}>
+                        <p>{el.description}</p>
+                        <p>{`Completed: ${el.completed}`}</p>
+                        <br/>
+                        <button
+                            onClick={()=>taskComplete(el.id)}
+                        >
+                            Completed
+                        </button>
+                    </li>
+                )}
+            </ul>
+    </>
+
+
+    )
 }
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
