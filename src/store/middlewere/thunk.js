2c6df2c540c94f8040bcf7ca94a8fdc54d45641e
Rostyslav Lisovyi <rokokos97@gmail.com>
2023-02-15 16:42:30 +0100

    thunk.js => processing error if action type is function

--- src/store/middlewere/thunk.js
+++ src/store/middlewere/thunk.js
@@ -1,9 +1,11 @@
 export function thunk(state) {
     return function wrapDispatch(next) {
         return function handleAction(action) {
-            console.log(next);
-            console.log(action);
-            return next(action)
+          if(typeof action === "function"){
+              console.log("function");
+          }else{
+              return next(action)
+          }
         }
     }
 }
--- src/store/store.js
+++ src/store/store.js
@@ -1,8 +1,9 @@
 import { createStore, compose, applyMiddleware } from "redux";
 import reducer from "./task";
 import {logger} from "./middlewere/logger";
+import {thunk} from "./middlewere/thunk";
 
-const middlewareEnhancer = applyMiddleware(logger)
+const middlewareEnhancer = applyMiddleware(logger,thunk)
 function configureStore () {
     return createStore(
         reducer,compose(middlewareEnhancer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()))
