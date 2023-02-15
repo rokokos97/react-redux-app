73300f93813bf2c62e5dc0ee7fa3ed9cbdf0fb97
Rostyslav Lisovyi <rokokos97@gmail.com>
2023-02-15 08:26:33 +0100

    store.js => change initialStore on configureStore and export default it

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React, {useEffect, useState} from 'react';
 import ReactDOM from 'react-dom/client';
 import * as actions from "./store/task/actions"
-import {initiateStore} from "./store/store";
+import configureStore from "./store/store";
 
 
 
-const store = initiateStore()
+const store = configureStore()
 const App = () => {
     const [state, setState] = useState(store.getState())
     useEffect(()=>{
--- src/store/store.js
+++ src/store/store.js
@@ -5,6 +5,7 @@ const initialState = [
     {id:1, title: "Task 1", completed: false},
     {id:2, title: "Task 2", completed: false}
 ]
-export function initiateStore () {
+function configureStore () {
     return createStore(reducer, initialState)
 }
+export default configureStore;
