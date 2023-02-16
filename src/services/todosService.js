8bb82bc7b09eb666664f2072c6ed267dfecd339b
Rostyslav Lisovyi <rokokos97@gmail.com>
2023-02-16 21:30:51 +0100

    httpService.js => add post: axios.post (6); todosService.js => add new method get

--- src/services/todosService.js
+++ src/services/todosService.js
@@ -9,6 +9,15 @@ const todosService = {
             }
         })
         return data
+    },
+    get: async () => {
+        const {data} = await httpService.post(todosEndpoint, {
+            params: {
+                title: "new title",
+                complete: false
+            }
+        })
+        return data;
     }
 }
 export default todosService;
