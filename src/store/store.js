d40606bb5044f8a19e6f7ff29cb22bce73c77567
Rostyslav Lisovyi <rokokos97@gmail.com>
2023-02-16 19:43:56 +0100

    add combineReducers

--- src/store/store.js
+++ src/store/store.js
@@ -1,10 +1,14 @@
 import reducer from "./task";
 import {logger} from "./middlewere/logger";
-import {configureStore} from "@reduxjs/toolkit"
-
+import {configureStore, combineReducers} from "@reduxjs/toolkit"
+import errorReducer from "./errors";
+const rootReducer = combineReducers({
+    error: errorReducer,
+    task: reducer
+})
 function createStore () {
     return configureStore({
-        reducer,
+        rootReducer,
         middleware: (getDefaultMiddleware)=>
             getDefaultMiddleware().concat(logger),
         devTools: process.env.NODE_ENV !== "production"
