0c8b5ae5750dffaea09b2457ecb8e69a1c1354c1
Rostyslav Lisovyi <rokokos97@gmail.com>
2023-02-16 18:49:07 +0100

    index.js => change [state] = useState to const state = useSelector((state)=>state)

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from 'react';
+import React, {useEffect} from 'react';
 import ReactDOM from 'react-dom/client';
 import configureStore from "./store/store";
 import {changedTitle, completeTask, deletedTitle, getTasks} from "./store/task";
